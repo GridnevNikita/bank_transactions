0a9bd46e25019932246f921998c146af4c3b6d89
Gridnev Nikita <gridnevnikit4@gmail.com>
2025-06-06 13:32:26 +0300

    fixed utils.py views.py

--- src/utils.py
+++ src/utils.py
@@ -4,25 +4,27 @@ from datetime import datetime, time
 
 def filter_transactions_by_date(transactions: List[Dict], date_str: str) -> List[Dict]:
     """
-    Фильтрует транзакции по дате.
-    Возвращает операции с начала месяца по указанную дату включительно (весь день).
+    Фильтрует транзакции по дате операции.
+    Принимает дату в формате 'ДД.ММ.ГГГГ'.
+    Возвращает операции с начала месяца по указанную дату включительно.
     """
-    input_date = datetime.strptime(date_str, "%Y-%m-%d")
 
-    # Начало месяца - первый день с 00:00:00
-    start_date = input_date.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
-
-    # Конец дня входящей даты — 23:59:59, чтобы включить все операции за этот день
+    input_date = datetime.strptime(date_str, "%d.%m.%Y")
+    start_date = datetime.combine(input_date.replace(day=1).date(), time.min)
     end_date = datetime.combine(input_date.date(), time.max)
 
     filtered_list = []
     for transaction in transactions:
-        # Преобразуем дату из транзакции из формата Excel ('ДД.ММ.ГГГГ ЧЧ:ММ:СС')
-        tx_date = datetime.strptime(transaction["Дата операции"], "%d.%m.%Y %H:%M:%S")
+        try:
+            raw_date = transaction["Дата операции"]
+            if not isinstance(raw_date, str):
+                continue
+            operation_date = datetime.strptime(raw_date, "%d.%m.%Y %H:%M:%S")
 
-        # Проверяем, что дата операции лежит в нужном интервале
-        if start_date <= tx_date <= end_date:
-            filtered_list.append(transaction)
+            if start_date <= operation_date <= end_date:
+                filtered_list.append(transaction)
+        except (KeyError, ValueError):
+            continue
 
     return filtered_list
 
@@ -32,8 +34,8 @@ if __name__ == "__main__":
 
     # Загружаем транзакции
     transactions = load_excel_transactions("../data/operations.xlsx")
-    date_str = "2021-12-31"
+    date_str = "31.12.2021"
     # Фильтруем транзакции
     filtered_tx = filter_transactions_by_date(transactions, date_str)
-    for tx in filtered_tx:
-        print(tx["Дата операции"], tx["Описание"])
+    for filtered in filtered_tx:
+        print(filtered["Дата операции"], filtered["Описание"])
--- src/views.py
+++ src/views.py
@@ -1,23 +1,82 @@
 from datetime import datetime
+from typing import List, Dict, Any
 
 
-def get_greeting(current_dt: datetime) -> str:
+def get_greeting() -> str:
     """Программа приветствует в зависимости от текущего времени."""
-    hour = current_dt.hour
-    if 6 <= hour < 12:
-        return "Доброе утро"
-    elif 12 <= hour < 18:
-        return "Добрый день"
-    elif 18 <= hour < 23:
-        return "Добрый вечер"
+    current_hour = datetime.now().hour
+    if 5 <= current_hour < 12:
+        return "Доброе утро!"
+    elif 12 <= current_hour < 18:
+        return "Добрый день!"
+    elif 18 <= current_hour < 23:
+        return "Добрый вечер!"
     else:
-        return "Доброй ночи"
+        return "Доброй ночи!"
+
+def get_cards_summary(transactions: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    """
+    Возвращает список словарей с данными по каждой карте.
+    """
+    cards_summary: Dict[str, float] = {}
+
+    for transaction in transactions:
+        if (
+                isinstance(transaction.get("Номер карты"), str)
+                and transaction.get("Статус") != "FAILED"
+                and isinstance(transaction.get("Сумма операции"), (int, float))
+                and transaction["Сумма операции"] < 0
+        ):
+            card = transaction["Номер карты"][-4:]
+            amount = -transaction["Сумма операции"]
+
+            cards_summary[card] = cards_summary.get(card, 0) + amount
+
+    result = []
+    for last_digits, total_spent in cards_summary.items():
+        result.append({
+            "last_digits": last_digits,
+            "total_spent": round(total_spent, 2),
+            "cashback": round(total_spent * 0.01, 2)  # 1% от расходов
+        })
+
+    return result
+
+
+from typing import List, Dict
+
+def get_top_transactions(transactions: List[Dict], top_n: int = 5) -> List[Dict]:
+    """
+    Возвращает топ-N транзакций с наибольшими расходами.
+    Учитываются только транзакции со статусом 'OK' и отрицательной суммой.
+    """
+    filtered = [
+        transaction for transaction in transactions
+        if transaction.get("Статус") == "OK"
+           and isinstance(transaction.get("Сумма операции"), (int, float))
+           and transaction["Сумма операции"] < 0
+    ]
+    sorted_tx = sorted(filtered, key=lambda i: abs(i["Сумма операции"]), reverse=True)
+    top_transactions = []
+    for i in sorted_tx[:top_n]:
+        top_transactions.append({
+            "date": i.get("Дата платежа", ""),
+            "amount": round(abs(i["Сумма операции"]), 2),
+            "category": i.get("Категория", ""),
+            "description": i.get("Описание", "")
+        })
+
+    return top_transactions
+
 
 
 if __name__ == "__main__":
-    test_times = ["2023-06-05 07:30:00", "2023-06-05 13:00:00", "2023-06-05 19:45:00", "2023-06-05 02:15:00"]
-
-    for time_str in test_times:
-        dt = datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S")
-        greeting = get_greeting(dt)
-        print(f"Время: {time_str} => Приветствие: {greeting}")
+    print(get_greeting())
+    from data_loader import load_excel_transactions
+    from utils import filter_transactions_by_date
+    transactions = load_excel_transactions("../data/operations.xlsx")
+    filtered_transactions = filter_transactions_by_date(transactions, "31.12.2021")
+    report = get_cards_summary(filtered_transactions)
+    top = get_top_transactions(filtered_transactions)
+    # print("Отчёт по картам:")
+    print(top)
